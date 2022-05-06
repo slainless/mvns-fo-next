59b050f209b4664f3d9e6bc5bfd2ae00534dbc89
inf-initely <aimanfauzy.99@gmail.com>
2022-05-06 10:12:19 +0800

    fix(type): minor type fix

--- global.d.ts
+++ global.d.ts
@@ -2,11 +2,4 @@ declare type ReactHTML = import('react').ReactHTML
 declare type HTMLAttributes<T> = import('react').HTMLAttributes<T>
 declare type HTMLAttr<T extends keyof ReactHTML> = Parameters<ReactHTML[T]>[0] &
   HTMLAttributes<HTMLElement>
-
-import 'react'
-
-declare module 'react' {
-  interface CSSProperties {
-    [key: `--${string}`]: string | number
-  }
-}
+declare type ReactProps<T> = Parameters<T>[0]
